5fcad39ec396038d3d01ae79dcbc7c74cec31380
tahaapuhan <81593068+tahaapuhan@users.noreply.github.com>
2022-09-14 23:20:28 +0300

    Add files via upload

---
+++ header.php
@@ -0,0 +1,71 @@
+
+<!-- HEADER START -->
+<!DOCTYPE html>
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="author" content="www.petstylist.com">
+    <meta name="copyright" content="Taha Apuhan">
+    <meta name="keywords" content="pendik pet kuaför,pet kuaför, kedi, köpek, kedi tıraşı, köpek tıraşı, kedi,tıraşı, köpek,tıraşı, pendik, kadıköy, pet, anestezisiz kedi tıraşı, anestezisiz,kedi,tıraşı, pet kuaför randevu">
+    <meta property="og:description" content="Pet Stylist Ceren hayvanlarınıza birer dost olmak için artık sizlerle... Pet Kuaför adına her şey burada">
+    <meta name="description" content="Pet Stylist Ceren hayvanlarınıza birer dost olmak için artık sizlerle... Pet Kuaför adına her şey burada">
+    <title>petstylistcero</title>
+    <link rel="stylesheet" href="nicepage.css" media="screen">
+    <link rel="stylesheet" href="Ev.css?v004" media="screen">
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-JMWJGBCPHN"></script>
+    <script class="u-script" type="text/javascript" src="jquery.js" defer=""></script>
+    <script class="u-script" type="text/javascript" src="nicepage.js" defer=""></script>
+    <meta name="generator" content="Nicepage 4.11.3, nicepage.com">
+    <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i">
+    <link id="u-page-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i|PT+Sans:400,400i,700,700i">
+    <meta name="theme-color" content="#478ac9">
+    <meta property="og:title" content="petstylistceren">
+    <meta property="og:type" content="website">
+    <meta name="google-site-verification" content="y-sIu48yUgs0X4DmhceQM5gLRu68gAUutLlF5VEoGYA" />
+    
+  </head>
+  <body class="u-body u-xl-mode">
+    
+    <header class="u-clearfix u-header u-header" id="sec-eb25">
+      <div class="u-clearfix u-sheet u-valign-middle u-sheet-1">
+        <div class="header-logo"><br>
+            <h2><a href="http://petstylistcero.com">pet stylist cero</a></h2>
+        </div>
+      <div class="ceren">
+          <h2>Ceren Baydeniz</h2>
+      </div>
+          
+          
+
+        <nav class="u-menu u-menu-dropdown u-offcanvas u-menu-1">
+          <div class="menu-collapse" style="font-size: 1rem; letter-spacing: 0px;">
+            <a class="u-button-style u-custom-left-right-menu-spacing u-custom-padding-bottom u-custom-top-bottom-menu-spacing u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="#">
+              <svg class="u-svg-link" viewBox="0 0 24 24"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#menu-hamburger"></use></svg>
+              <svg class="u-svg-content" version="1.1" id="menu-hamburger" viewBox="0 0 16 16" x="0px" y="0px" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg"><g><rect y="1" width="16" height="2"></rect><rect y="7" width="16" height="2"></rect><rect y="13" width="16" height="2"></rect>
+</g></svg>
+            </a>
+          </div>
+          <div class="u-nav-container">
+            <ul class="u-nav u-unstyled u-nav-1"><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="index.php" style="padding: 10px 20px;">Anasayfa</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="#about" style="padding: 10px 20px;">Hakkımızda</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="#videos" style="padding: 10px 20px;">Videolarım</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="#iletisim" style="padding: 10px 20px;">İletişim</a>
+</li></ul>
+          </div>
+          <div class="u-nav-container-collapse">
+            <div class="u-black u-container-style u-inner-container-layout u-opacity u-opacity-95 u-sidenav">
+              <div class="u-inner-container-layout u-sidenav-overflow">
+                <div class="u-menu-close"></div>
+                <ul class="u-align-center u-nav u-popupmenu-items u-unstyled u-nav-2"><li class="u-nav-item"><a class="u-button-style u-nav-link" href="index.php">Anasayfa</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="#about">Hakkımızda</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="#videos">Videolarım</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="#iletisim">İletişim</a>
+</li></ul>
+              </div>
+            </div>
+            <div class="u-black u-menu-overlay u-opacity u-opacity-70"></div>
+          </div>
+        </nav>
+      </div>
+    </header>
+<!-- HEADER FİNİSH -->
---
+++ index.php
@@ -0,0 +1,147 @@
+<?php include 'header.php' ?>
+
+    <section class="u-clearfix u-image u-valign-middle u-section-1" id="carousel_ce2d" data-image-width="1980" data-image-height="1193">
+      <div class="u-clearfix u-layout-wrap u-layout-wrap-1">
+        <div class="u-layout">
+          <div class="u-layout-row">
+            <div class="u-align-left u-container-style u-layout-cell u-left-cell u-size-23-xl u-size-26-lg u-size-26-md u-size-26-sm u-size-26-xs u-layout-cell-1">
+              <div class="u-container-layout u-valign-middle u-container-layout-1">
+                <h1 class="u-custom-font u-font-raleway u-text u-text-body-alt-color u-text-1"> PET KUAFÖR NEDİR?<br>
+                </h1>
+                <p class="u-text u-text-palette-2-light-3 u-text-2"> 
+                  Kedi ve köpek gibi evde bakıldığında bakım ihtiyacı duyan evcil hayvanların yıkanma, tırnak kesimleri, tüy kesimleri ve tüy bakımları işlemlerini pet kuaförleri yapar. Evcil hayvanlar, evde bakıldığında tıraş ve tırnak kesimi gibi düzenli olarak yapılması gereken bakım ihtiyaçlarına sahiptir.
+              </p>
+              
+                <a href="https://api.whatsapp.com/send?phone=905530640395&text=Merhaba%20Pet%20Kuaf%C3%B6r%20hizmetleriniz%20i%C3%A7in%20bilgi%20almak%20istiyorum" class="u-active-white u-border-s u-btn u-btn-rectangle u-button-style u-custom-font u-font-pt-sans u-hover-white u-palette-2-dark-3 u-radius-0 u-text-active-palette-2-dark-3 u-text-body-alt-color u-text-hover-palette-2-dark-3 u-btn-2">Randevu Al</a>
+              </div>
+            </div>
+            <div class="u-container-style u-image u-layout-cell u-right-cell u-shape-rectangle u-size-34-lg u-size-34-md u-size-34-sm u-size-34-xs u-size-37-xl u-image-1" data-image-width="1348" data-image-height="1000">
+              <div class="u-container-layout u-container-layout-2"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+   <style>
+
+   </style>
+    <div id="yukari">&#8679</div>
+
+    <section class="u-clearfix u-palette-2-base u-valign-middle-lg u-valign-middle-sm u-valign-middle-xl u-valign-middle-xs u-section-3" id="carousel_2640">
+      <div class="u-opacity u-opacity-40 u-palette-2-light-1 u-shape u-shape-rectangle u-shape-1"></div>
+      <div class="u-clearfix u-layout-wrap u-layout-wrap-1">
+        <div class="u-layout">
+          <div class="u-layout-row">
+            <div class="u-align-left u-container-style u-layout-cell u-left-cell u-size-31 u-layout-cell-1">
+              <div class="u-container-layout u-valign-middle u-container-layout-1">
+                <BR></BR>
+                <h1 class="u-custom-font u-font-raleway u-text u-text-1"> PET TIRAŞININ ÖNEMİ?</h1>
+                <p class="u-text u-text-palette-2-light-3 u-text-2">Hayvan severlerin kedi ve köpeklerini tıraş ettirme gereksinimleri ırklarına, cinslerine ve mevsimlere göre değişiklik göstermektedir. Örneğin; uzun tüye sahip olan kedi ya da köpeklerin tüylerinde uzama ve yapışma söz konusu olduğunda derilerde ciddi yaralanmalar ve hijyen problemleri ortaya çıkmaktadır. Bu nedenle tıraş olmaları zorunluluk gösterir. Soğuk iklimlerde yaşayan hayvanlar sıcak iklimlerde kürklerinden kurtulmazsa kesin olarak pişme sorunu yaşar. Pet tıraşı sayesinde sadık dostlarımızın çok daha konforlu bir şekilde yaşamaları mümkün olmaktadır.</p>
+                <BR></BR>
+                <h1 class="u-custom-font u-font-raleway u-text u-text-1"> PET TIRAŞINDA DİKKAT EDİLMESİ GEREKENLER !</h1>
+                <p class="u-text u-text-palette-2-light-3 u-text-2">Pet kuaför hizmetleri altında yapılan işlemlerden biri ped tıraşıdır. Bakıldığında yalnızca profess
+        ller tarafından yapılması gerektiğinin öneminin altını çizmekte fayda var. Hayvanların tıraş öncesinde derilerinin dikkatle incelenmesi, yara ya da parazitlerin olup olmadığına bakılması gerekmektedir. Farklı sağlık kontrollerinin yapılması sonrasında olumlu sonuçlanması halinde pet tıraşı yapılabilmektedir.</p>
+              </div>
+            </div>
+            <div class="u-align-center u-container-style u-layout-cell u-right-cell u-size-29 u-layout-cell-2">
+              <div class="u-container-layout u-valign-middle u-container-layout-2">
+                <div class="u-image u-image-circle u-image-1" data-image-width="800" data-image-height="720"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="u-clearfix u-palette-2-light-2 u-section-4" id="carousel_73f3">
+      <div class="u-clearfix u-layout-wrap u-layout-wrap-1" id="videos">
+        <div class="u-layout">
+
+        <div class="video-title">
+           <h2>BULUT BEYLE EĞLENCELİ DAKİKALAR 🐕</h2>
+        </div>
+
+        <div class="videos">
+        <video width="500" height="300" controls="controls" style="background: black; border-radius: 10px"> 
+          <source src="images/video1.mp4" type="video/mp4">
+        </video>
+        </div>
+
+        <div class="video-title">
+            <h2>ANESTEZİSİZ KEDİ TIRAŞI OLUR MU? BEN YAPARSAM OLUR 💪</h2>
+          </div>
+
+        <div class="videos">
+        <video width="500" height="300" controls="controls" style="background: black; border-radius: 10px"> 
+          <source src="images/video2.mp4" type="video/mp4">
+        </video>
+        </div>
+
+        <div class="video-title">
+            <h2>MERHABAA BANA ÇOK SORULAN KÖPEK YIKAMA VİDEOSU İLE GELDİM 🥰</h2>
+          </div>
+          
+        <div class="videos">
+        <video width="500" height="300" controls="controls" style="background: black; border-radius: 10px"> 
+          <source src="images/video3.mp4" type="video/mp4">
+        </video>
+        </div>
+
+          </div>
+        </div>
+      </div>
+    </section>
+   
+    <section class="u-align-center u-clearfix u-palette-2-base u-section-6">
+      <div class="u-clearfix u-sheet u-valign-middle-md u-valign-middle-sm u-valign-middle-xs u-sheet-1">
+        <div class="u-clearfix u-expanded-width u-layout-wrap u-layout-wrap-1">
+          <div class="u-layout">
+            <div class="u-layout-row">
+              <div class="u-container-style u-layout-cell u-left-cell u-size-30 u-layout-cell-1">
+                <div class="u-container-layout u-valign-middle u-container-layout-1">
+                  <h2 class="u-custom-font u-font-raleway u-text u-text-body-alt-color u-text-1" id="about">Hakkımda</h2>
+                  <p class="u-text u-text-2">Cras adipiscing enim eu turpis egestas pretium aenean pharetra magna. Tempus urna et pharetra pharetra massa massa ultricies mi quis. Ut faucibus pulvinar elementum <a href="https://nicepage.com" class="u-active-s
+         u-border-1 u-border-white u-btn u-button-link u-button-style u-hover-s
+         u-s
+         u-text-body-alt-color u-btn-1">integer</a> enim neque volutpat. Augue mauris augue neque gravida in. Venenatis cras sed felis eget velit aliquet sagittis. Morbi leo urna molestie at.<br>
+                  </p>
+                </div>
+              </div>
+
+              <div class="u-container-style u-layout-cell u-right-cell u-size-30 u-layout-cell-2">
+                <div class="u-container-layout u-valign-middle-lg u-valign-middle-xl u-container-layout-2">
+                  <div class="u-opacity u-opacity-40 u-palette-2-light-1 u-shape u-shape-circle u-shape-1"></div>
+                  <div alt="" class="u-image u-image-circle u-image-1"></div>
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    
+<?php include 'footer.php' ?>
+
+<?php include 'baglanti.php';
+
+if(isset($_POST["adsoyad"], $_POST["email"], $_POST["mesaj"])) {
+
+  $adsoyad=$_POST["adsoyad"];
+  $email=$_POST["email"]; 
+  $mesaj=$_POST["mesaj"]; 
+
+  $ekle="INSERT INTO ceren (adsoyad, email, mesaj) VALUES
+  ('".$adsoyad."','".$email."', '".$mesaj."')";
+
+  if($baglan->query($ekle)===TRUE) {
+    echo "<script>alert('Mesajınız başarı ile gönderildi.')</script>";
+  }
+}
+
+
+
+?>
+
+
